4e61f2f722b8c426aa97aa04d1084f40b4d57a91
Hiroyuki Sano <sh19910711@gmail.com>
2015-04-15 16:33:41 +0900

    ex02: fix myshell_template

--- ex02/myshell_template.c
+++ ex02/myshell_template.c
@@ -12,15 +12,15 @@
 
 void  parse(char *line, char **argv)
 {
-     while (*line != '\0') {       /* if not the end of line ....... */ 
-          while (*line == ' ' || *line == '\t' || *line == '\n')
-               *line++ = '\0';     /* replace white spaces with 0    */
-          *argv++ = line;          /* save the argument position     */
-          while (*line != '\0' && *line != ' ' && 
-                 *line != '\t' && *line != '\n') 
-               line++;             /* skip the argument until ...    */
-     }
-     *argv = '\0';                 /* mark the end of argument list  */
+  while (*line != '\0') {       /* if not the end of line ....... */ 
+    while (*line == ' ' || *line == '\t' || *line == '\n')
+      *line++ = '\0';     /* replace white spaces with 0    */
+    *argv++ = line;          /* save the argument position     */
+    while (*line != '\0' && *line != ' ' && 
+        *line != '\t' && *line != '\n') 
+      line++;             /* skip the argument until ...    */
+  }
+  *argv = '\0';                 /* mark the end of argument list  */
 }
 
 /* ----------------------------------------------------------------- */
@@ -30,52 +30,50 @@ void  parse(char *line, char **argv)
 /* this function forks a child process to execute the command using  */
 /* system call execvp().                                             */
 /* ----------------------------------------------------------------- */
-     
+
 void  execute(char **argv)
 {
-     pid_t  pid;
-     int    status;
-     
-     if ((pid = fork()) < 0) {     /* fork a child process           */
-          printf("*** ERROR: forking child process failed\n");
-          exit(1);
-     }
-     else if (pid == 0) {          /* for the child process:         */
-		 /* add your code here, execute command using execvp function */
+  pid_t  pid;
+  int    status;
 
-
-           
+  if ((pid = fork()) < 0) {     /* fork a child process           */
+    printf("*** ERROR: forking child process failed\n");
+    exit(1);
+  }
+  else if (pid == 0) {          /* for the child process:         */
+    /* add your code here, execute command using execvp function */
 
 
 
-               printf("*** ERROR: exec failed\n");
-               exit(1);
-          }
-     }
-     else {                                  /* for the parent:      */
-          while (wait(&status) != pid)       /* wait for completion  */
-               ;
-     }
+
+
+
+    printf("*** ERROR: exec failed\n");
+    exit(1);
+  } else {                                  /* for the parent:      */
+    while (wait(&status) != pid)       /* wait for completion  */
+      ;
+  }
 }
 
 /* ----------------------------------------------------------------- */
 /*                  The main program starts here                     */
 /* ----------------------------------------------------------------- */
-     
+
 int  main(void)
 {
-     char  line[1024];             /* the input line                 */
-     char  *argv[64];              /* the command line argument      */
-     
-     while (1) {                   /* repeat until done ....         */
-          printf("Shell -> ");     /*   display a prompt             */
-          fgets(line,sizeof(line),stdin);              /*   read in the command line     */
-          printf("\n");
-          parse(line, argv);       /*   parse the line               */
-          if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
-               exit(0);            /*   exit if it is                */
-          execute(argv);           /* otherwise, execute the command */
-     }
+  char  line[1024];             /* the input line                 */
+  char  *argv[64];              /* the command line argument      */
+
+  while (1) {                   /* repeat until done ....         */
+    printf("Shell -> ");     /*   display a prompt             */
+    fgets(line,sizeof(line),stdin);              /*   read in the command line     */
+    printf("\n");
+    parse(line, argv);       /*   parse the line               */
+    if (strcmp(argv[0], "exit") == 0)  /* is it an "exit"?     */
+      exit(0);            /*   exit if it is                */
+    execute(argv);           /* otherwise, execute the command */
+  }
 }
 
-                
+

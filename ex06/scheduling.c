45cda5345a478d9aaf94736168715028548aa8d7
Hiroyuki Sano <sh19910711@gmail.com>
2015-05-27 16:58:16 +0900

    ex06: fix indent

--- ex06/scheduling.c
+++ ex06/scheduling.c
@@ -5,12 +5,12 @@
 
 /* Process Data Structure */
 struct process {
-    int pid;                /* Process ID */
-    int burst;              /* CPU Burst Time, total CPU time required */
-    int priority;           /* Priority */
-    int working;            /* Working time, for round-robin scheduling */
-    int waiting;            /* Waiting time, for round-robin scheduling */
-    struct process *next;
+  int pid;                /* Process ID */
+  int burst;              /* CPU Burst Time, total CPU time required */
+  int priority;           /* Priority */
+  int working;            /* Working time, for round-robin scheduling */
+  int waiting;            /* Waiting time, for round-robin scheduling */
+  struct process *next;
 };
 
 /* Function Prototype Declarations */
@@ -21,84 +21,84 @@ void rr (struct process *proc, int quantum);
 
 /* Main Program Segment */
 int main (void) {
-    /* Initialize process list */
-    struct process *plist, *ptmp;
-    plist       = init_process(1, 10, 3);
-    plist->next = init_process(2,  1, 1); ptmp = plist->next;
-    ptmp->next  = init_process(3,  2, 3); ptmp = ptmp->next;
-    ptmp->next  = init_process(4,  1, 4); ptmp = ptmp->next;
-    ptmp->next  = init_process(5,  5, 2);
+  /* Initialize process list */
+  struct process *plist, *ptmp;
+  plist       = init_process(1, 10, 3);
+  plist->next = init_process(2,  1, 1); ptmp = plist->next;
+  ptmp->next  = init_process(3,  2, 3); ptmp = ptmp->next;
+  ptmp->next  = init_process(4,  1, 4); ptmp = ptmp->next;
+  ptmp->next  = init_process(5,  5, 2);
 
-    /* Perform simulations */
-    listprocs(plist);
-    fcfs(plist);
-    rr(plist, 1);
+  /* Perform simulations */
+  listprocs(plist);
+  fcfs(plist);
+  rr(plist, 1);
 
-    /* Terminate cleanly */
-    while (plist != NULL) {
-        ptmp = plist;
-        plist = plist->next;
-        free(ptmp);
-    };
-    return(0);
+  /* Terminate cleanly */
+  while (plist != NULL) {
+    ptmp = plist;
+    plist = plist->next;
+    free(ptmp);
+  };
+  return(0);
 };
 
 
 /* Process list entry initialization routine */
 struct process *init_process (int pid, int burst, int priority) {
-    struct process *proc;
-    proc = malloc(sizeof(struct process));
-    if (proc == NULL) {
-        printf("Fatal error: memory allocation failure.\nTerminating.\n");
-        exit(1);
-    };
-    proc->pid = pid;
-    proc->burst = burst;
-    proc->priority = priority;
-    proc->working = 0;
-    proc->waiting = 0;
-    proc->next = NULL;
-    return(proc);
+  struct process *proc;
+  proc = malloc(sizeof(struct process));
+  if (proc == NULL) {
+    printf("Fatal error: memory allocation failure.\nTerminating.\n");
+    exit(1);
+  };
+  proc->pid = pid;
+  proc->burst = burst;
+  proc->priority = priority;
+  proc->working = 0;
+  proc->waiting = 0;
+  proc->next = NULL;
+  return(proc);
 };
 
 
 /* First-Come-First-Served scheduling simulation */
 void fcfs (struct process *proc) {
-    int time = 0, start, end;
-    struct process *tmp = proc;
+  int time = 0, start, end;
+  struct process *tmp = proc;
 
-    printf("BEGIN:\tFirst-Come-First-Served scheduling simulation\n");
+  printf("BEGIN:\tFirst-Come-First-Served scheduling simulation\n");
 
-    while (tmp != NULL) {
-        start = time;
-		
-		//add your code here
+  while (tmp != NULL) {
+    start = time;
 
-
+    //add your code here
 
 
 
-        end = time;
-        printf("Process: %d\tEnd Time: %d\tWaiting: %d\tTurnaround: %d\n", tmp->pid, time, start, end);
-        tmp = tmp->next;
-    };
 
-    printf("END:\tFirst-Come-First-served scheduling simulation\n\n");
+
+    end = time;
+    printf("Process: %d\tEnd Time: %d\tWaiting: %d\tTurnaround: %d\n", tmp->pid, time, start, end);
+    tmp = tmp->next;
+  };
+
+  printf("END:\tFirst-Come-First-served scheduling simulation\n\n");
 };
 
 
 /* Process listing */
 void listprocs (struct process *proc) {
-    struct process *tmp = proc;
+  struct process *tmp = proc;
 
-    printf("BEGIN:\tProcess Listing\n");
+  printf("BEGIN:\tProcess Listing\n");
 
-    while (tmp != NULL) {
-        printf("PID: %d\t\tPriority: %d\tBurst: %d\n", tmp->pid, tmp->priority, tmp->burst);
-        tmp = tmp->next;
-    };
+  while (tmp != NULL) {
+    printf("PID: %d\t\tPriority: %d\tBurst: %d\n", tmp->pid, tmp->priority, tmp->burst);
+    tmp = tmp->next;
+  };
 
-    printf("END:\tProcess Listing\n\n");
+  printf("END:\tProcess Listing\n\n");
 };
 
 /* Round-Robin scheduling simulation */
@@ -145,7 +145,7 @@ void rr (struct process *proc, int quantum) {
   current = proc;
   while (current != NULL) {
     printf("Process: %d\tWorking: %d\tWaiting: %d\tTurnaround: %d\n",
-           current->pid, current->working, current->waiting, current->working + current->waiting);
+        current->pid, current->working, current->waiting, current->working + current->waiting);
     current = current->next;
   };
 
